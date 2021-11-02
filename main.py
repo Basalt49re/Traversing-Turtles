08b6eb86d4957c4d9225691698360c98a4a8ad39
Albert Yang <70829217+Basalt49re@users.noreply.github.com>
2021-11-02 17:26:48 +0000

    almost final

--- main.py
+++ main.py
@@ -8,31 +8,30 @@ my_turtles = []
 
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
-turtle_colors = ["gold", "purple", "orange", "green", "blue", "red"]
+turtle_colors = ["red", "green", "orange", "purple", "blue", "gold"]
 
 for s in turtle_shapes:
   t = trtl.Turtle(shape=s)
   t.penup()
   t.color(turtle_colors.pop())
-  t.pencolor("black")
-  my_t
+  t.pencolor(turtle_colors.pop())
+  my_turtles.append(t)
  
 #  
 startx = 0
 starty = 0
-startDir = -45
+startDir = 90
 #
 for t in my_turtles:
   t.goto(startx, starty)  
   t.pendown()  
   t.setheading(startDir)
+  t.right(45)
   t.forward(50)
-  
-
 #	
-  startx = t.xcor + 50
-  starty = t.ycor + 50
-  startDir += 45
+  startx = t.xcor()
+  starty = t.ycor()
+  startDir = t.heading()
 
-wn = t.Screen()
+wn = trtl.Screen()
 wn.mainloop()
