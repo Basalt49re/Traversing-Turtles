1422195930bb094d398b035d08ecee906eb89b28
Albert Yang <70829217+Basalt49re@users.noreply.github.com>
2021-11-02 17:18:40 +0000

    set t as main variable and definition

--- main.py
+++ main.py
@@ -1,36 +1,37 @@
 #   a117_traversing_turtles.py
 #   Add code to make turtles move in a circle and change colors.
-import turtle as trtl
+import turtle as t
 
 # create an empty list of turtles
 my_turtles = []
-my_colors = []
-direction = 45
+
 
 # use interesting shapes and colors
 turtle_shapes = ["arrow", "turtle", "circle", "square", "triangle", "classic"]
 turtle_colors = ["gold", "purple", "orange", "green", "blue", "red"]
 
 for s in turtle_shapes:
-  t = trtl.Turtle(shape=s)
-  my_turtles.append(t)
-
+  t.shape(s)
+  my_turtles.append(s)
+  t.penup()
+  t.color(turtle_colors.pop())
+  t.append(t)
 #  
 startx = 0
 starty = 0
-
+startDir = -45
 #
 for t in my_turtles:
   t.goto(startx, starty)    
   #t.right(45)
-  t.setheading(direction)
+  t.setheading(startDir)
   t.forward(50)
-  direction = t.heading()
+  
 
 #	
   startx = startx + 50
   starty = starty + 50
-  direction = direction - 45
+  startDir = startDir + 45
 
 wn = trtl.Screen()
 wn.mainloop()
